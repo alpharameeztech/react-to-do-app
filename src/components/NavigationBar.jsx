5a51bb70a06113ee3a84bd3cd085cff29e40785d
Rameez Israr <rameez@govassist.com>
2024-08-19 16:47:23 +0500

    Add react router

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -1,13 +1,21 @@
 import React from 'react'
+import {NavLink, Route} from "react-router-dom";
+import About from "./pages/About";
 
 export default function NavigationBar() {
   return (
     <div>
         <nav>
             <ul>
-                <li><a href='#'>Home</a></li>
-                <li><a href='#'>About</a></li>
-                <li><a href='#'>Contact</a></li>
+                <li>
+                    <NavLink to='/' className={({ isActive }) => (isActive ? 'active' : '')}>Home</NavLink>
+                </li>
+                <li>
+                    <NavLink to='/about'className={({ isActive }) => (isActive ? 'active' : '')}>About</NavLink>
+                </li>
+                <li>
+                    <NavLink to='/contact' className={({ isActive }) => (isActive ? 'active' : '')}>Contact</NavLink>
+                </li>
             </ul>
         </nav>
     </div>
--- src/components/Root.jsx
+++ src/components/Root.jsx
@@ -2,15 +2,23 @@ import React from 'react'
 import App from "./App";
 import About from "./pages/About";
 import NavigationBar from "./NavigationBar";
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import Contact from "./pages/Contact";
 
 export default function Root() {
   return (
-      <div className="todo-app-container">
-        <NavigationBar />
-          <div className="content">
-              <App />
-              {/*<About />*/}
+      <Router>
+          <div className="todo-app-container">
+            <NavigationBar />
+              <div className="content">
+                  <Routes>
+                      <Route path="/" element={<App />} />
+                      <Route path="/about" element={<About />} />
+                      <Route path="/contact" element={<Contact />} />
+                  </Routes>
+              </div>
           </div>
-      </div>
+      </Router>
+
   )
 }
